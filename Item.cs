32df13cdfd1627661d6b45567eecea604572d882
choijiho0925 <jiho.0925.choi@gmail.com>
2025-04-23 17:48:42 +0900

    add : enum JobOption
    Item.cs 안에 enum.JobOption 생성

--- Item.cs
+++ Item.cs
@@ -8,6 +8,7 @@ namespace TeamRpg
 {
     public class Item
     {
+        public JobOption Type { get; set; }
         public string Job { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
@@ -18,7 +19,7 @@ namespace TeamRpg
         public bool isBuy;
         public bool isEquipped;
 
-        public Item(string job, string name, string description, int attack, int defense, int gold, bool isBuyItem, bool isEquippedItem)
+        public Item(JobOption type, string job, string name, string description, int attack, int defense, int gold, bool isBuyItem, bool isEquippedItem)
         {
             Job = job;
             Name = name;
@@ -31,5 +32,35 @@ namespace TeamRpg
             isBuy = isBuyItem;
             isEquipped = isEquippedItem;
         }
+
+        public static string StringOption(JobOption type)
+        {
+            if (type == JobOption.Gladiator)
+            {
+                return "검투사";
+            }
+            else if (type == JobOption.Hunter)
+            {
+                return "수렵꾼";
+            }
+            else if (type == JobOption.Assassin)
+            {
+                return "암살자";
+            }
+            else if (type == JobOption.Amor)
+            {
+                return "방어구";
+            }
+            else
+            {
+                return "물약";
+            }
+        }
+    }
+
+    public enum JobOption
+    {
+        Gladiator, Hunter, Assassin, Amor, Potion
     }
 }
+
